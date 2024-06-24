dd2d6ae4362ecbd60b40ed9a97fe597654379c67
DESKTOP-MF9HU5V\ADMIN <jaysukh.devotee@gmail.com>
2024-06-24 18:21:23 +0530

    Basics of JavaScript:1.Introduction to JavaScript 2.Syntax of JS 3.Variables 4.Data Types 5.Operators basic example.

---
+++ 1_basic/1_intro.js
@@ -0,0 +1,3 @@
+console.log("Introduction to JavaScript");
+console.log("==============================");
+console.log("JavaScript is a scripting language that enables you to create dynamically updating content, control multimedia, animate images, and much more.");
---
+++ 1_basic/4_datatyape.js
@@ -0,0 +1,25 @@
+console.log("Data Types:1.String 2.Number 3.Bigint 4.Boolean 5.Undefined 6.Null 7.Object");
+console.log("==============");
+let str = "Hello";
+let num = 450;
+let bigInt = 123456789;
+let bool = true;
+let undef;
+let nullVar = null;
+let obj = {name: "jay", age: 22};
+console.log("String = " +  str);
+
+console.log("Number = " + num);
+
+console.log("Bigint = " + bigInt);
+
+console.log("Boolean = " + bool);
+
+console.log("Undefined = " + undef);
+
+console.log("Null = " + nullVar);
+
+console.log("Object = " + JSON.stringify(obj));
+
+
+
