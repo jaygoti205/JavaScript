8da29564a629985ad463c851412e9eabc3658be4
jaygoti205 <jaysukh.devotee@gmail.com>
2024-07-17 09:27:03 +0530

    Delete 2_ control_structures directory

--- 2_ control_structures/10_hosting.js
+++
@@ -1,9 +0,0 @@
-console.log("Hosting");
-console.log("===========================");
-x = 10;//Declaration and initialization
-y = 20; 
-console.log(x); //10
-var x;//Declaration 
-//let y; //ReferenceError: Cannot access 'y' before initialization
-
-
